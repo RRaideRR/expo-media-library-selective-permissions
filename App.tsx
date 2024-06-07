fca65c29325078693b0981c7aa8248c998ab756a
philipp <philipp.reiter90@gmail.com>
2024-06-07 15:11:57 +0200

    init commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Platform, StyleSheet, Text, View } from 'react-native';
+import * as MediaLibrary from 'expo-media-library';
+import { useEffect, useState } from 'react';
 
 export default function App() {
+  const [permissionResponse, requestPermission] = MediaLibrary.usePermissions();
+
+  useEffect(() => {
+    if (permissionResponse?.granted) {
+      MediaLibrary.addListener((listener) => {
+        console.log('assets changed');
+      })
+
+      return () => {
+        MediaLibrary.removeAllListeners();
+      }
+    }
+
+  }, [permissionResponse?.granted]);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      {!!permissionResponse?.granted ?
+      <Button
+        title="Select more photos"
+        onPress={() => {
+        if (Platform.OS === 'ios') {
+          MediaLibrary.presentPermissionsPickerAsync();
+          return;
+        }
+
+        if (Platform.OS === 'android') {
+          requestPermission();
+        }
+      }}
+      /> :
+        <Button
+          title="Request Permission"
+          onPress={requestPermission}
+        />
+      }
     </View>
   );
 }
